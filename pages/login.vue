3412b296a09eeaeddbbcd7758848448aa2f3da58
cheezytony <antonio@casava.co>
2024-01-02 21:33:18 +0100

    Remove crypto package

--- pages/login.vue
+++ pages/login.vue
@@ -8,7 +8,12 @@ const { signIn, signOut, session, status } = useAuth();
 
 <template>
   <div class="grid h-[100dvh] -mt-20 place-items-center">
-    <Button color-scheme="pink" left-icon="arrow-right-to-bracket" is-rounded @click="signIn">
+    <Button
+      color-scheme="pink"
+      href="/api/auth/signin"
+      left-icon="arrow-right-to-bracket"
+      is-rounded
+    >
       Click to sign in
     </Button>
   </div>
